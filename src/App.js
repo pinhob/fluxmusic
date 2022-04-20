a20525c9e3cc2aa7bb56895ec9a7b0e6989ade0e
Bruno Pinho <brunopinho@outlook.com>
2022-04-20 16:38:12 -0300

    feat: create home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Homepage } from './pages';
 
 function App() {
   return (
-    <h1>FluxMusic</h1>
+    <>
+      <h1>FluxMusic</h1>
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </>
   );
 }
 
