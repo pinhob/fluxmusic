3efad3333e5045e883b6d27d9783ffda86693590
Bruno Pinho <brunopinho@outlook.com>
2022-04-22 22:56:56 -0300

    chore: change album image

--- src/components/AlbumsCards.js
+++ src/components/AlbumsCards.js
@@ -1,11 +1,11 @@
 import { StyledAlbumCard } from "../styles";
-import georgeHarrison from "../assets/images/george_harrison.png";
+import freakOutCover from "../assets/images/freak_out_album.jpg";
 
 const AlbumsCards = ({ albums }) => {
   return (
     albums && albums.map((album) => (
       <StyledAlbumCard key={album.id}>
-        <img src={georgeHarrison} alt="Album cover" className="c-card__img" />
+        <img src={freakOutCover} alt="Album cover" className="c-card__img" />
         <h2 className="c-card__title">{album.title}</h2>
         <h3 className="c-card__year">{album.release_date.split('-')[0]}</h3>
       </StyledAlbumCard>))
